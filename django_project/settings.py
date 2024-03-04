fb3972abb6d9d68cb2781ddc707057c098967208
dudubarbaro <eduardobarbaro.ifc@gmail.com>
2024-03-04 16:45:12 -0300

    Update CORS_ALLOWED_ORIGINS and CSRF_TRUSTED_ORIGINS in settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -59,7 +59,9 @@ CORS_ALLOWED_ORIGINS = (
     "http://localhost:8000",
 )
 
-CSRF_TRUSTED_ORIGINS = ["localhost:3000"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 ROOT_URLCONF = "django_project.urls"
 
